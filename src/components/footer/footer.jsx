9a894ef8dac867ab353dee920c5642ae20084e8b
rahmonovsaydullo <saydullo779@gmail.com>
2025-08-10 14:10:40 +0500

    I changed skills bg color

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -9,27 +9,27 @@ import {
 
 const Footer = () => {
   return (
-    <footer id="contact" className=" text-gray-900 py-12 px-6 lg:px-20 border-t border-gray-700">
+    <footer id="contact" className="bg-gray-900 text-gray-300 py-12 px-6 lg:px-20 border-t border-gray-700">
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-10">
         
         {/* Contact Section */}
         <div>
-          <h2 className="text-xl font-bold mb-4 text-dark">Contact</h2>
+          <h2 className="text-xl font-bold mb-4 text-white">Contact</h2>
           <ul className="space-y-2 text-sm">
             <li className="flex items-center gap-3">
               <FaEnvelope className="text-blue-400" />
-              <a href="mailto:saydullo779@gmail.com" className="hover:underline text-gray-900">saydullo779@gmail.com</a>
+              <a href="mailto:saydullo779@gmail.com" className="hover:underline text-gray-300">saydullo779@gmail.com</a>
             </li>
             <li className="flex items-center gap-3">
               <FaPhone className="text-green-400" />
-              <a href="tel:+998942203989" className="hover:underline text-gray-900">+998 94 220 39 89</a>
+              <a href="tel:+998942203989" className="hover:underline text-gray-300">+998 94 220 39 89</a>
             </li>
           </ul>
         </div>
 
         {/* Social Media */}
         <div>
-          <h2 className="text-xl font-bold mb-4 text-dark">Social</h2>
+          <h2 className="text-xl font-bold mb-4 text-white">Social</h2>
           <div className="flex gap-6 text-2xl">
             <a href="https://t.me/you_can_contact" target="_blank" rel="noopener noreferrer" title="Telegram">
               <FaTelegramPlane className="text-[#0088cc] hover:scale-110 transition-transform duration-200" />
@@ -38,7 +38,7 @@ const Footer = () => {
               <FaLinkedin className="text-[#0077B5] hover:scale-110 transition-transform duration-200" />
             </a>
             <a href="https://github.com/rahmonovsaydullo" target="_blank" rel="noopener noreferrer" title="GitHub">
-              <FaGithub className="text-dark hover:scale-110 transition-transform duration-200" />
+              <FaGithub className="text-white hover:scale-110 transition-transform duration-200" />
             </a>
           </div>
         </div>
