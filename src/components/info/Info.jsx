32f94807fd9d582f687c20901c2da3da9f62838f
rahmonovsaydullo <saydullo779@gmail.com>
2025-08-31 14:08:51 +0500

    I updated resume

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -34,7 +34,7 @@ function Info() {
           {/* Download Resume Button */}
           <div className="relative group w-fit mx-auto lg:mx-0">
             <a
-              href="/SaydullojonRahmonov.pdf"
+              href="/rakhmonov_saydulojon_resume.pdf"
               download
               title="Click to download resume"
               className="inline-block bg-blue-600 text-white px-6 py-3 rounded-xl shadow-md hover:bg-blue-700 transition cursor-pointer"
